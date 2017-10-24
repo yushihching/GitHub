2b8d5f9c61ab2d9a1c3d1034c416189fe97e5f2e
yushihching <yushihching@gmail.com>
2017-10-25 01:14:27 +0800

    Initial commit

---
+++ Seller/js/main.js
@@ -0,0 +1,8 @@
+
+        $(function(){
+            $('[data-toggle="tooltipNews"]').tooltip(
+                {title: "<div><a href='#'>房市新聞</a></div><div><a href='#'>促銷資訊</a></div><div><a href='#'>系統公告</a></div>",
+                 html: true,
+                 delay:{"show":100,"hide":1000
+             }});
+        });
